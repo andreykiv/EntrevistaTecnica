d1fc445aa1e917c2030b3d186d56732f094c0632
Myroslav <andreykiv09@gmail.com>
2021-03-14 17:54:46 +0100

    db: inserts simplificados

--- db/dbEntrevistaTecnica.sql
+++ db/dbEntrevistaTecnica.sql
@@ -64,41 +64,49 @@ CREATE TABLE candidatePosition (
 );
 
 -- dumping data
-insert into positions (title, dateCreation, descriptionPosition)values('Programador Java Junior', now(), 'Puesto estable!!!');
-insert into positions (title, dateCreation, descriptionPosition)values('Programador JavaScript Junior', now(), 'Puesto estable!!!');
-insert into positions (title, dateCreation, descriptionPosition)values('Programador Java Mid', now(), 'Puesto estable!!!');
-insert into positions (title, dateCreation, descriptionPosition)values('Programador Python Junior', now(), 'Puesto estable!!!');
-insert into positions (title, dateCreation, descriptionPosition)values('Programador C# Junior', now(), 'Puesto INestable!!!');
 
-INSERT INTO skill(skillName)VALUES('SQL');
-INSERT INTO skill(skillName)VALUES('JAVA');
-INSERT INTO skill(skillName)VALUES('PHP');
-INSERT INTO skill(skillName)VALUES('HTML');
-INSERT INTO skill(skillName)VALUES('CSS');
+insert into positions (title, dateCreation, descriptionPosition)values
+('Programador Java Junior', now(), 'Puesto estable!!!'),
+('Programador JavaScript Junior', now(), 'Puesto estable!!!'),
+('Programador Java Mid', now(), 'Puesto estable!!!'),
+('Programador Python Junior', now(), 'Puesto estable!!!'),
+('Programador C# Junior', now(), 'Puesto estable!!!');
 
-insert into companyUser(username, pwd, userRole) values('neo', '123asdasjd', 'expert');
-insert into companyUser(username, pwd, userRole) values('trinity', '123ajsdasd', 'HR user');
-insert into companyUser(username, pwd, userRole) values('morpheo', '123jjasdasd', 'expert');
-insert into companyUser(username, pwd, userRole) values('agent smith', '123asdasjjd', 'expert');
-insert into companyUser(username, pwd, userRole) values('architect', '123assddasd', 'HR user');
+insert into skill(skillName)values
+('SQL'),
+('JAVA'),
+('PHP'),
+('HTML'),
+('CSS');
 
-insert into candidate (username, pwd, nameCandidate, surname) values ("sisco", "589746", "Paco", "Casanova");
-insert into candidate (username, pwd, nameCandidate, surname) values ("maricarmen", "45454ds", "Rogelio", "Valls");
-insert into candidate (username, pwd, nameCandidate, surname) values ("slav", "545854", "Tom", "Monich");
-insert into candidate (username, pwd, nameCandidate, surname) values ("jose", "589746", "Maria", "Jesus");
-insert into candidate (username, pwd, nameCandidate, surname) values ("rintintin", "10550", "Pepe@", "Chateau");
+insert into companyUser(username, pwd, userRole) values
+('neo', '123asdasjd', 'expert'),
+('trinity', '123ajsdasd', 'HR user'),
+('morpheo', '123jjasdasd', 'expert'),
+('agent smith', '123asdasjjd', 'expert'),
+('architect', '123assddasd', 'HR user');
 
-insert into candidatePosition(idCandidate, idPosition, registryDate, testDate, completionDate, result) values(2, 1, now(), now(), now(), 9.4);
-insert into candidatePosition(idCandidate, idPosition, registryDate, testDate, completionDate, result) values(2, 3, now(), now(), now(), 4.4);
-insert into candidatePosition(idCandidate, idPosition, registryDate, testDate, completionDate, result) values(2, 2, now(), now(), now(), 4.4);
-insert into candidatePosition(idCandidate, idPosition, registryDate, testDate, completionDate, result) values(1, 1, now(), now(), now(), 7.4);
-insert into candidatePosition(idCandidate, idPosition, registryDate, testDate, completionDate, result) values(1, 4, now(), now(), now(), 8.4);
+insert into candidate (username, pwd, nameCandidate, surname) values
+("sisco", "589746", "Paco", "Casanova"),
+("maricarmen", "45454ds", "Rogelio", "Valls"),
+("slav", "545854", "Tom", "Monich"),
+("jose", "589746", "Maria", "Jesus"),
+("rintintin", "10550", "Pepe@", "Chateau");
 
-insert into candidateSkill(idCandidate, idSkill, notes, skillValue) values(2, 5, 'high skill', 7.7);
-insert into candidateSkill(idCandidate, idSkill, notes, skillValue) values(4, 2, 'low skill', 4.7);
-insert into candidateSkill(idCandidate, idSkill, notes, skillValue) values(2, 2, 'mid skill', 5.7);
-insert into candidateSkill(idCandidate, idSkill, notes, skillValue) values(2, 3, 'over skill', 3.7);
-insert into candidateSkill(idCandidate, idSkill, notes, skillValue) values(1, 2, 'no skill', 8.7);
+insert into candidatePosition(idCandidate, idPosition, registryDate, testDate, completionDate, result) values
+(2, 1, now(), (now() + interval 2 hour), (now() + interval 3 hour), 9.4),
+(2, 3, now(), (now() + interval 2 hour), (now() + interval 3 hour), 4.4),
+(2, 2, now(), (now() + interval 2 hour), (now() + interval 3 hour), 5.4),
+(1, 4, now(), (now() + interval 2 hour), (now() + interval 3 hour), 3.4),
+(3, 3, now(), (now() + interval 2 hour), (now() + interval 3 hour), 7.4);
+
+
+insert into candidateSkill(idCandidate, idSkill, notes, skillValue) values
+(4, 2, 'high skill', 7.7),
+(2, 2, 'mid skill', 4.7),
+(2, 1, 'high skill', 3.7),
+(1, 5, 'high skill', 7.7),
+(4, 5, 'over skill', 9.7);
 
 
 
