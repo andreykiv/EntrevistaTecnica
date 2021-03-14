bb79c1e8217f26e381a0a4547ebead6d0719470c
Myroslav <andreykiv09@gmail.com>
2021-03-14 17:49:03 +0100

    esqueleto proyecto Spring

---
+++ src/main/java/entrevista_tecnica/EntrevistaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package entrevista_tecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrevistaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrevistaTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/entrevista_tecnica/EntrevistaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package entrevista_tecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrevistaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
