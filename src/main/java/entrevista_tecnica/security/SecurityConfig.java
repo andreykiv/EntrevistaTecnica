a5cf78bf61d9f4c733ae49b2134d765a1bc8abe3
Myroslav <andreykiv09@gmail.com>
2021-03-18 16:43:11 +0100

    CORS config implementada

--- src/main/java/entrevista_tecnica/security/SecurityConfig.java
+++ src/main/java/entrevista_tecnica/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package entrevista_tecnica.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -39,4 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			   .roles("USER");
        
     }
+   
+   	//IMPLEMENTAMOS  CORS POLICY PARA TODAS LAS RUTAS y METODOS?
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 }
