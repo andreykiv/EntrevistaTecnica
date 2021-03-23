f3a5b65d49703a1dda68d1e6715bc6afd767f810
Gerard <gerardbonetbustos@gmail.com>
2021-03-23 22:47:37 +0100

    login/forgot

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotComponent } from './login/forgot/forgot.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path:'login/forgot', component:ForgotComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
